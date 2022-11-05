dfb1b292b63fa7018f21815695c40880abb2fc83
nshimiyimano-cloud <njeanluc828@gmail.com>
2022-11-05 13:15:15 +0200

    first mivie controller and pass in params

---
+++ src/Controller/MoviesController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MoviesController extends AbstractController
+{
+    /**
+     * @Route("/movies/{name}", name="movies")
+     */
+    
+    public function index($name): JsonResponse
+    {
+        return $this->json([
+            'message' => $name,
+            'path' => 'src/Controller/MoviesController.php',
+        ]);
+    }
+}
