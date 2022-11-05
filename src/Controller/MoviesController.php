b7b130569c2d555328a4ddbf1c8a7bd56af6ef99
nshimiyimano-cloud <njeanluc828@gmail.com>
2022-11-05 14:23:12 +0200

    connect controller with view from template folder

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -3,20 +3,17 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MoviesController extends AbstractController
 {
     /**
-     * @Route("/movies/{name}", name="movies")
+     * @Route("/movies", name="movies")
      */
     
-    public function index($name): JsonResponse
+    public function index(): Response
     {
-        return $this->json([
-            'message' => $name,
-            'path' => 'src/Controller/MoviesController.php',
-        ]);
-    }
+        return $this->render('index.html.twig');
+}
 }
